a70b568468b67f6680c80264633b1dd4d3bf397a
Isaque <isaquedossantoscosta@hotmail.com>
2025-03-28 08:36:49 -0300

    Initial commit

---
+++ cadastro.py
@@ -0,0 +1,16 @@
+
+nome = input ("Digite o seu nome:")
+idade = float(input("Digite sua idade:"))
+sexo = input ("Digite a sua sexo:")
+rua = input("Digite sua rua:")
+bairro =input ("Digite seu bairro:")
+cidade =input ("Digite sua cidade:")
+
+print("------------------------")
+print("O SEU NOME É", nome)
+print("A SUA IDADE É", idade)
+print("O SEU SEXO É", sexo)
+print("O SEU BAIRRO É", bairro)
+print("O SUA CIDADE É", cidade)
+
+
---
+++ calculadora.py
@@ -0,0 +1,11 @@
+# Calculadora simples de dois numeros!
+# Solitando as operações
+
+num1 = float (input("Digite o Primeiro numero;"))
+num2 = float(input("Digite o segundo numero"))
+operador = input("Digite operação(+,-,*,/)")
+
+# Realizando as operações
+
+if operador == '+':
+    resultado = num1 + num2
