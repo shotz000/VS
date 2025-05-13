684aa684f109a822e27b5639289d27436cc27310
Isaque <isaquedossantoscosta@hotmail.com>
2025-05-13 10:20:01 -0300

    att

---
+++ exemplo_vetor.py
@@ -0,0 +1,14 @@
+#Em Python,não precisamos declarar o tipo e tamanho fixo
+#do vetor antecipadamente
+
+
+
+#Cria uma lista com 4 elementos,inicialmente com 0
+vetorDeInteiros = [0] * 4 #vetor que vai do indice 0 até 4
+
+
+vetorDeInteiros[0] = 2
+
+
+#Usamos f-strings para formatar a saída de forma concisa.
+print(f"Na posição 0, o valor é.: {vetorDeInteiros}")
---
+++ idade.py
@@ -0,0 +1,54 @@
+# inicializando a lista com 8 elementos
+IDADE = [0] * 8
+
+
+# Atribui strings (nomes) a cada
+# posição da lista
+
+IDADE[0] = "Joana de souza"
+IDADE[1] = "Pedro costa"
+IDADE[2] = "Jaqueline Almeida"
+IDADE[3] = "Sandro Ferreira"
+IDADE[4] = "Carlos souto"
+IDADE[5] = "Bianca silva"
+IDADE[6] = "Alair Guimarães"
+IDADE[7] = "Alessandra Fernanades"
+
+
+# print imprimir cada nome
+# da lista uma linha separada.
+
+
+print(IDADE[0])
+print(IDADE[1])
+print(IDADE[2])
+print(IDADE[3])
+print(IDADE[4])
+print(IDADE[5])
+print(IDADE[6])
+print(IDADE[7])
+
+
+#Criamos uma lista de tamanho 4.
+#Os elementos serão booleanos.
+
+#Inicializa a lista com 4 elementos False.
+VetorDelogico = [False] * 4
+
+
+#Atribui o valor booleano True
+#á primeira posição da lista (indice 0).
+VetorDelogico[0] = True
+
+#Usamos a f-strings para formatar a saída,inserindo 0 
+#valor da variavel.
+
+
+print("Na posição 0, o valor é.:(vetorDelogico[0])")
+print("Na posição 1, o valor é.:(vetorDelogico[1])")
+print("Na posição 2, o valor é.:(vetorDelogico[2])")
+print("Na posição 4, o valor é.:(vetorDelogico[4])")
+print("Na posição 5, o valor é.:(vetorDelogico[5])")
+print("Na posição 6, o valor é.:(vetorDelogico[6])")
+print("Na posição 7, o valor é.:(vetorDelogico[7])")
+
