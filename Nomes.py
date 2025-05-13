bf5141e17c4f849e8b056ed73936367cfbc4e483
Isaque <isaquedossantoscosta@hotmail.com>
2025-05-13 10:55:30 -0300

    att

---
+++ Nomes.py
@@ -0,0 +1,29 @@
+# inicializando a lista com 8 elementos
+IDADE = [0] * 8
+
+
+# Atribui strings (nomes) a cada
+# posição da lista
+
+IDADE[0] = "Joana de souza"
+IDADE[1] = "Pedro costa"
+IDADE[2] = "Jaqueline Almeida"
+IDADE[3] = "Sandro Ferreira"
+IDADE[4] = "Carlos souto"
+IDADE[5] = "Bianca silva"
+IDADE[6] = "Alair Guimarães"
+IDADE[7] = "Alessandra Fernanades"
+
+
+# print imprimir cada nome
+# da lista uma linha separada.
+
+
+print(IDADE[0])
+print(IDADE[1])
+print(IDADE[2])
+print(IDADE[3])
+print(IDADE[4])
+print(IDADE[5])
+print(IDADE[6])
+print(IDADE[7])
