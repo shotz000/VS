2c114ff73382035e1e47d286bddea35156b21f6f
Isaque <isaquedossantoscosta@hotmail.com>
2025-05-15 11:11:47 -0300

    att

---
+++ desafio.py
@@ -0,0 +1,20 @@
+MATRIZ = [[0] * 4 for _ in range(4)]
+
+
+MATRIZ[0][0] = 1("Digite o 1:")
+MATRIZ[0][1] = 2("Digite o 2:")
+MATRIZ[0][2] = 3("Digite 0 3:")
+MATRIZ[0][3] = 4("Digite o 4:") 
+MATRIZ[0][4] = 5("Digite o 5:")
+MATRIZ[0][5] = 6("Digite o 6:")
+MATRIZ[0][6] = 7("Digite o 7:")
+MATRIZ[0][7] = 8("Digite o 8:")
+
+print()
+
+MATRIZ[1][0] = 9("Digite o nunmero 9:")
+MATRIZ[1][1] = 10("Digite o numero 10:")
+MATRIZ[1][2] = 11("Digite o numero 11:")
+MATRIZ[1][3] = 12("Digite o numero 12:")
+MATRIZ[1][4] = 13("Digite o numero 13:")
+MATRIZ[1][5] = 14("Digite o nuemro 14:")
