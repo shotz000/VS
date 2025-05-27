29263371e48eb7d7b2b6e5a2a103764862d429ba
Isaque <isaquedossantoscosta@hotmail.com>
2025-05-27 08:11:57 -0300

    Create att01.py

---
+++ att01.py
@@ -0,0 +1,24 @@
+print("Cafeteria de BH")
+print("----------------")
+print("")
+print("****TIPOS DE CAFÉ****")
+print(" ")
+print("Café Simples")
+print("Café expresso")
+print("Café Moderno")
+print("")
+print("*** PREÇOS ***")
+print("")
+print("CAFÉ SIMPLES R$7.00")
+print("CAFÉ EXPRESSO R$10.00")
+print("CAFÉ MODERNO R$15.00")
+print("_______________________")
+print("DIGITE O TIPO DE CAFÉ:")
+
+
+tipocafé = input()
+
+if tipocafé = print("VOCE ESCOLHEU O CAFÉ SIMPLES = R$7.00")
+
+print("QUANTOS CAFÉ VOCE VAI QUERER?")
+quantcafe = float(input())
